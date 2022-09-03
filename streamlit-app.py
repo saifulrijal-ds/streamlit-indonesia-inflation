43647b67227906a4ab9f0fedc636061fcfabe835
Saiful Rijal <46024788+saifulrijal-ds@users.noreply.github.com>
2022-09-03 22:54:59 +0700

    update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,7 +4,20 @@ import pandas as pd
 
 @st.cache
 def get_data():
-    return pd.read_pickle("data-streamlit/inflation_bps.pkl")
+    df =  pd.read_pickle("data-streamlit/inflation_bps.pkl")
+    return df.melt(id_vars=["date", "bulan", "tahun"])
 
 df = get_data()
 st.dataframe(df)
+
+calculation_type=st.multiselect(
+    ["Year on Year", "Month to Month", "Year to date"],
+    ["Year on Year"]
+)
+
+calculation_type_map = {
+    "Year on Year": "yoy",
+    "Month to Month": "mtm",
+    "Year to date": "ytd"
+}
+
