cf883238c110b5ebdf6ec79c283b0b4cdeb48ca8
Saiful Rijal <46024788+saifulrijal-ds@users.noreply.github.com>
2022-09-03 23:28:19 +0700

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,7 +8,7 @@ def get_data():
     return df.melt(id_vars=["date", "bulan", "tahun"])
 
 df = get_data()
-st.dataframe(df)
+# st.dataframe(df)
 
 calculation_type=st.multiselect(
     "Silakan pilih bagaimana inflasi dihitung:",
@@ -23,4 +23,21 @@ calculation_type_map = {
 }
 
 selected_df = df[df["variable"].isin([calculation_type_map[ct] for ct in calculation_type])]
-st.dataframe(selected_df)
+# st.dataframe(selected_df)
+
+color_range = ["#1f77b4", "#ff7f0e", "#2ca02c"]
+domain = ["yoy", "mtm", "ytd"]
+
+inflation_chart = alt.Chart(selected_df).mark_line().encode(
+    x=alt.X("date:T", title="Periode"),
+    y=alt.Y("value:Q", title="Tingkat Inlfasi (%)"),
+    color=alt.Color("variable:O", scale=alt.Scale(domain=domain, range=color_range)),
+    tooltip=[
+        alt.Tooltip("date", title="Periode"),
+        alt.Tooltip("value", title="Tingkat Inflasi (%)", format=".2f")
+    ]
+).properties(
+    title="Tingkat Inflasi Indonesia"
+)
+
+st.altair_chart(inflation_chart, use_container_width=True)
