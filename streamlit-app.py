60ba5bbf378abadeb83d5c7360e18322851634f6
Saiful Rijal <46024788+saifulrijal-ds@users.noreply.github.com>
2022-09-04 00:03:02 +0700

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,6 +10,16 @@ def get_data():
 df = get_data()
 # st.dataframe(df)
 
+first_date = df["date"].min()
+last_date = df["date"].max()
+
+start_date = st.sidebar.date_input("Tanggal mulai", first_date)
+end_date = st.sidebar.date_input('Tanggal akhir', last_date)
+if start_date < end_date:
+    st.success(f"Start date: {start_date} \nEnd date: {end_date}")
+else:
+    st.error('Error: End date must fall after start date.')
+
 calculation_type=st.multiselect(
     "Silakan pilih bagaimana inflasi dihitung:",
     ["Year on Year", "Month to Month", "Year to date"],
@@ -23,13 +33,14 @@ calculation_type_map = {
 }
 
 selected_df = df[df["variable"].isin([calculation_type_map[ct] for ct in calculation_type])]
-# st.dataframe(selected_df)
+st.dataframe(selected_df)
 
 color_range = ["#1f77b4", "#ff7f0e", "#2ca02c"]
 domain = ["yoy", "mtm", "ytd"]
 
+date_scale = [start_date, end_date]
 inflation_chart = alt.Chart(selected_df).mark_line().encode(
-    x=alt.X("date:T", title="Periode", timeUnit="yearmonth"),
+    x=alt.X("date:T", title="Periode", scale=date_scale),
     y=alt.Y("value:Q", title="Tingkat Inlfasi (%)"),
     color=alt.Color("variable:O", scale=alt.Scale(domain=domain, range=color_range), title="Perhitungan"),
     tooltip=[
